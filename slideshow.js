03f99e62df60051bae6ada30456194ef60009135
DenddyRizky <denddyrizky@gmail.com>
2023-09-28 10:51:05 +0200

    made script into a javascript

---
+++ slideshow.js
@@ -0,0 +1,20 @@
+var index = 0;
+var rotationTime = 3000;
+slideShow();
+
+function slideShow() {
+    var currentSlide;
+    var slides = document.getElementsByClassName("slide");
+
+    for (currentSlide = 0; currentSlide < slides.length; currentSlide++) {
+        slides[currentSlide].style.display = "none";
+    }
+    index++;
+    if (index > slides.length) {
+        index = 1;
+    }
+    slides[index - 1].style.display = "block";
+    setTimeout(slideShow, rotationTime);
+
+}
+// Bron  staat in bronvermelding
